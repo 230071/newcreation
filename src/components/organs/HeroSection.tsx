e04cf2fd16f633c8b8e45439d96a8c85915593d8
230071 <kojokipeacemaria@gmail.com>
2025-10-17 19:15:25 +0300

    update

--- src/components/organs/HeroSection.tsx
+++ src/components/organs/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
   return (
     <section 
       className="w-full lg:h-screen md:h-[550px] h-[830px] relative overflow-x-hidden flex justify-end bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${bgImage})` }}
+      // style={{ backgroundImage: `url(${bgImage})` }}
     >
       <main className="w-full lg:h-full h-auto grid md:grid-cols-2 absolute top-0 left-0 lg:px-24 md:px-8 px-5 pt-24 md:pt-32 lg:pt-0">
         <div className="flex flex-col justify-center md:gap-6 gap-3 md:order-1 order-2">
